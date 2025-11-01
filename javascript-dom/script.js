ce7c2ad6f325ec9e2c30913c2d800e8f6970501b
Sufal Thakre <sufalthakre1@gmail.com>
2025-11-01 04:01:23 +0000

    prctice elements

--- javascript-dom/script.js
+++ javascript-dom/script.js
@@ -27,3 +27,36 @@
 // h1.textContent="Jai hanuman"
 // h1.innerHTML='<i>jai rajputana<i/>'
 // ---------------------------
+
+// getAttribute, removeAttribute, setAttribute
+// let img=document.querySelector('img')
+// img.setAttribute('src','https://english.cdn.zeenews.com/sites/default/files/2023/03/03/1162130-fpvc3scaiaat6fw.jpg')
+// img.removeAttribute('alt')
+// console.log(img.getAttribute('src'));
+ 
+// ---------------------------
+// createElement, append, prepend,appendChild,remove
+// let h2=document.createElement('h2')
+// h2.innerText="safety"
+// document.querySelector('div').append(h2)
+// document.querySelector('div').prepend(h2)
+// document.querySelector('div').appendChild(h2)
+// document.querySelector('div').remove()
+
+// --------------------------------
+
+// .style, .classList (add. remove, toggle)
+
+// let div=document.querySelector('div')
+// let h1= div.querySelector('h1')
+// h1.style.color="red"
+// h1.style.textAlign="center"
+// h1.style.textTransform="Capitalize"
+// h1.style.background="grey"
+
+// h1.classList.add('mag')
+// h1.classList.remove('mag')
+// h1.classList.toggle('mag')
+
+// --------------------------------
+
