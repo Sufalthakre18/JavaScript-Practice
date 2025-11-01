652d6348a234bf5a3efa81dca858f61c8601614d
Sufal Thakre <sufalthakre1@gmail.com>
2025-11-01 01:01:43 +0000

    dom manuplation

---
+++ javascript-dom/script.js
@@ -0,0 +1,29 @@
+// selecting elements
+
+// let el=document.getElementById('abc')
+// console.log(el);
+// console.dir(el);
+
+// let el=document.getElementsByClassName('abc')
+// console.log(el);
+// console.dir(el);
+
+// let el=document.querySelectorAll('h1')
+// console.log(el);
+// console.dir(el);
+
+// let el=document.querySelector('h1')
+// console.log(el);
+// console.dir(el);
+// --------------------------
+
+// DOM  TREE STRUCTURE: Node,Element,Text,Comment
+// -----------------------
+
+// let h1= document.querySelector('h1')
+// console.log(h1);
+
+// h1.innerText="Jai shree ram"
+// h1.textContent="Jai hanuman"
+// h1.innerHTML='<i>jai rajputana<i/>'
+// ---------------------------
