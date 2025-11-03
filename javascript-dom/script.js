1380f0e7ab4ed02d3ef07bfb3cd1daf3238936dd
Sufal Thakre <sufalthakre1@gmail.com>
2025-11-03 12:01:42 +0000

    event

--- javascript-dom/script.js
+++ javascript-dom/script.js
@@ -33,7 +33,7 @@
 // img.setAttribute('src','https://english.cdn.zeenews.com/sites/default/files/2023/03/03/1162130-fpvc3scaiaat6fw.jpg')
 // img.removeAttribute('alt')
 // console.log(img.getAttribute('src'));
- 
+
 // ---------------------------
 // createElement, append, prepend,appendChild,remove
 // let h2=document.createElement('h2')
@@ -70,12 +70,12 @@
 // let li=document.querySelectorAll('li')
 // li.forEach((val)=>{
 //     console.log(val.textContent);
-    
+
 // })
- 
+
 // for(let i=0;i<li.length;i++){
 //     console.log(li[i].innerText);
-    
+
 // }
 
 // ----------------
@@ -118,45 +118,167 @@
 
 // inp.addEventListener('input',function(detls){
 //     console.log(detls);
-    
+
 // })
 
 // inp.addEventListener('input',function(detls){
 //     console.log(detls.data);
-    
+
 // })
 // inp.addEventListener('input',function(detls){
 //     if(detls.data!==null) console.log(detls.data);
-    
+
 // })
 // --------------------
-
+// CHANGE
 // let h1= document.querySelector('h1')
 // let select=document.querySelector('select')
 // select.addEventListener('change',(detls)=>{
 //     let car=detls.target.value
 //     h1.textContent=`${car.toUpperCase()} car selected`
-    
+
 // })
 
 // -------------------------
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
 
 
-h1=document.querySelector('h1')
-window.addEventListener('keydown',(detls)=>{
-    if(detls.key===' ') h1.textContent='SPC'
-    else h1.textContent=detls.key
-    h1.style.color=`${getRandomColor()}`
-    
-    
-    
-})
+// h1=document.querySelector('h1')
+// window.addEventListener('keydown',(detls)=>{
+//     if(detls.key===' ') h1.textContent='SPC'
+//     else h1.textContent=detls.key
+//     h1.style.color=`${getRandomColor()}`
+
+
+
+// })
+
+// --------------------
+
+// let btn=document.querySelector('.btn')
+// let input=document.querySelector('input')
+
+// btn.addEventListener('click',(dtl)=>{
+//   input.click()
+// })
+
+// input.addEventListener('change',(dtl)=>{
+//   let file=dtl.target.files[0]
+//   if (file) {
+//     btn.textContent=file.name
+//   }
+
+// })
+
+// -----------------------
+// SUBMIT
+// let inputs = document.querySelectorAll('input')
+// let form = document.querySelector('form')
+// let body = document.querySelector('body')
+
+
+// form.addEventListener('submit', (e) => {
+//     console.log(e);
+//     e.preventDefault()
+
+//     let img = document.createElement('img')
+//     img.setAttribute('src', inputs[0].value)
+
+//     let card = document.createElement('div')
+//     card.classList.add('card')
+//     card.appendChild(img)
+
+//     let h1 = document.createElement('h1')
+//     h1.textContent = inputs[1].value
+
+//     let h4 = document.createElement('h4')
+//     h4.textContent = inputs[2].value
+
+//     let p = document.createElement('p')
+//     p.textContent = inputs[3].value
+
+//     let main = document.createElement('main')
+
+
+//     main.appendChild(card)
+//     main.appendChild(h1)
+//     main.appendChild(h4)
+//     main.appendChild(p)
+
+//     body.appendChild(main)
+//     inputs.forEach((inp) => {
+//         if (inp.type !== 'submit') {
+//             inp.value = ''
+//         }
+//     })
+// })
+// ------------------------------------
+
+// MOUSEOVER MOUSEOUT MOUSEMOVE KEYUP
+
+// let div=document.querySelector('div')
+
+// div.addEventListener('mouseover',()=>{
+//     div.style.backgroundColor="yellow"
+// })
+// div.addEventListener('mouseout',()=>{
+//     div.style.backgroundColor="brown"
+// })
+
+// window.addEventListener('mousemove',(dtl)=>{
+
+//     div.style.top= dtl.clientY+"px"
+//     div.style.left= dtl.clientX+"px"
+
+
+// })
+
+// const input = document.getElementById("username");
+// const feedback = document.getElementById("feedback");
+
+// input.addEventListener("keyup", function () {
+//   const count = input.value.length;
+//   feedback.textContent = `Characters typed: ${count}`;
+// });
+
+
+// ---------------------------------
+// EVENT BUBBLING AND EVENT CAPTURING
+
+// BUBBLING -Event Bubbling ka matlab hai — jab hum kisi child element pe click karte hain, toh wo event pehle us child pe trigger hota hai, phir uske parent pe, phir uske parent ke parent pe... aise upar jaata hai. Jaise bubble paani mein upar uthta hai 
+
+// let ul=document.querySelector('ul')
+// ul.addEventListener('click',(e)=>{
+//   e.target.classList.toggle("lt")
+// })
+
+// CAPTURING--Event Capturing ka matlab hai — jab koi event sabse pehle parent element pe trigger hota hai, phir andar ke child elements tak jaata hai. Jaise ek security check gate se andar jaate jaate har level pe check hota hai 
+
+// ul.addEventListener('click',(e)=>{
+//   e.target.classList.toggle("lt")
+// },true) // element.addEventListener("click", function, true); // true = capturing mode
+
+// --------------------------------------
+
+// let input=document.querySelector('input')
+// let span=document.querySelector('span')
+
+// input.addEventListener('input',(dtl)=>{
+//   let left= 20-dtl.target.value.length
+//   span.textContent=left
+//   if (left<0) {
+//     span.style.color='red'
+//   }
+//   else{
+//     span.style.color='white'
+//   }
+  
+// })
